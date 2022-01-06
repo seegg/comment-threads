d0912e23bf253a0447052a82a5889c3912735f34
yuan tan <dyxtan@gmail.com>
2022-01-06 19:48:28 +1300

    add route for comment thread

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -7,12 +7,22 @@ module.exports = router;
 router.use('/:id', (req, res) => {
   const id = Number(req.params.id);
   console.log('comment id', id)
-  db.getComment(id).
-    then(result => {
-      console.log(result);
+  db.getComment(id)
+    .then(result => {
       res.status(200).json(result).send();
     })
     .catch(err => {
       console.error(err);
     });
+})
+
+router.use('/thread/:id', (req, res) => {
+  const id = Number(req.params.id);
+  db.getCommentThread(id)
+    .then(result => {
+      res.status(200).json(result).send();
+    })
+    .catch(err => {
+      console.log(err);
+    })
 })
